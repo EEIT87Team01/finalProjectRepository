84620d9927d02df45375b42f81cc63d24467b547
柏維 <@Bowei>
2016-09-27 19:49:50 +0800

    修正CalendarController Detail轉址功能

--- runninglife/src/main/java/iii/runninglife/controller/statistics/calendar/CalendarController.java
+++ runninglife/src/main/java/iii/runninglife/controller/statistics/calendar/CalendarController.java
@@ -45,7 +45,6 @@ import iii.runninglife.model.sporthistorypath.SportHistoryPathVO;
 
 @RestController
 @RequestMapping("/calendar")
-@SessionAttributes("membersVO")
 public class CalendarController{
 	@Autowired
 	SportHistoryService sportHistoryService;
@@ -55,8 +54,6 @@ public class CalendarController{
 	ChallDataCRUDService challDataCRUDService;
 	@Autowired
 	RunnerService runnerService;
-	@Autowired
-	ChallsCRUDService challsService;
 	@Autowired
 	MembersInterface memberDAO;
 	
@@ -73,15 +70,11 @@ public class CalendarController{
 	}
 	
 	@RequestMapping(value="get_calendar_json_data",method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-	public String getCalendarJsonData(@ModelAttribute MembersVO membersVO,HttpServletRequest req){
+	public String getCalendarJsonData(@RequestParam String memberID,@RequestParam String start,@RequestParam String end){
 		
 		List<CalendarVO> eventList = new ArrayList<>();
-		String memberID = req.getParameter("memberID");
-		System.out.println(memberID);
-		System.out.println(membersVO.getMemberID() + "hello");
-		Date startDate = Date.valueOf(req.getParameter("start"));
-		Date endDate = Date.valueOf(req.getParameter("end"));
-		String contextPath = req.getContextPath();
+		Date startDate = Date.valueOf(start);
+		Date endDate = Date.valueOf(end);
 		
 		Iterator<SportHistoryVO> sportHistorys = sportHistoryService
 							.getDataByMemberDurationDate(memberID,startDate,endDate)
@@ -96,7 +89,7 @@ public class CalendarController{
 			event.setEnd(sportHistoryVO.getEndDateTime().toString());
 			event.setTitle("運動紀錄");
 			event.setColor("YellowGreen"); 
-			event.setUrl(contextPath + "/calendar/sport_history.do?recordID=" + sportHistoryVO.getRecordID());
+			event.setUrl( "calendar/sport_history.do?recordID=" + sportHistoryVO.getRecordID());
 			 
 			eventList.add(event);
 		}
@@ -114,9 +107,7 @@ public class CalendarController{
 			event.setEnd(challDataVO.getChallDataPK().getChallenID().getChallenEndTime().toString());
 			event.setTitle(challDataVO.getChallDataPK().getChallenID().getChallenName());
 			event.setColor("DeepSkyBlue"); 
-			event.setUrl(contextPath + 
-					"/calendar/challen.do?challenID=" + challDataVO.getChallDataPK().getChallenID().getChallenID() + 
-					"&memberID=" + challDataVO.getChallDataPK().getMemberID().getMemberID());
+			event.setUrl("calendar/challen.do?challenID=" + challDataVO.getChallDataPK().getChallenID().getChallenID());
 			 
 			eventList.add(event);
 		}
@@ -134,9 +125,8 @@ public class CalendarController{
 			event.setEnd(runnerVO.getContest().getStartDate().toString());
 			event.setTitle(runnerVO.getContest().getContestName());
 			event.setColor("IndianRed"); 
-			event.setUrl(contextPath + 
-					"/calendar/race.do?contestID=" + runnerVO.getPk().getContestID());
-			System.out.println("contest added");
+			event.setUrl("calendar/race.do?contestID=" + runnerVO.getPk().getContestID());
+			
 			eventList.add(event);
 		}
   
@@ -167,20 +157,15 @@ public class CalendarController{
 	}
 	
 	@RequestMapping(value="/challen",method = RequestMethod.GET)
-	public ModelAndView getChallenDetailView(@RequestParam String challenID,@RequestParam String memberID){
-		 
-		ChallDataPK challDataPK = new ChallDataPK();
-		challDataPK.setChallenID(challsService.searchOneService(challenID));
-		challDataPK.setMemberID(memberDAO.selectOne(memberID));
-		ChallDataVO challDataVO = challDataCRUDService.searchOneService(challDataPK);
-		
-		return new ModelAndView("challenge/challDetail","challDataVO",challDataVO);
+	public ModelAndView getChallenDetailView(@RequestParam String challenID){
+		 		
+		return new ModelAndView("redirect:/challenge/detail/" + challenID + ".do");
 	}
 	
 	@RequestMapping(value="/race",method = RequestMethod.GET)
 	public ModelAndView getRaceDetailView(@RequestParam String contestID){
 
-		return new ModelAndView("redirect:/contest/contestID=" + contestID + ".do");
+		return new ModelAndView("redirect:/contest/" + contestID + ".do");
 	}
 	
 	private String formatDateTime(Timestamp dateTime){
